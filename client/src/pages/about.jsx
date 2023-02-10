0253fe219e363b1ad632dac637dacd43f6e774d2
Sajjad Anwar <sajjadanwar400@gmail.com>
2023-02-10 21:16:11 +0000

    fixing

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -10,7 +10,7 @@ import {
     GitHubIcon,
     LinkedInIcon,
 } from '@/components/SocialIcons'
-import portraitImage from '@/images/portrait.jpg'
+import portraitImage from '@/images/portrait.jpeg'
 
 function SocialLink({className, href, children, icon: Icon}) {
     return (
