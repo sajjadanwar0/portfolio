0c8ebd89c93a6fc677c21fbab23053ca8d924d31
Sajjad Anwar <sajjadanwar400@gmail.com>
2023-02-10 22:06:06 +0000

    fixing

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -66,30 +66,12 @@ export default function About() {
                         </h1>
                         <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
                             <p>
-                                I’ve loved making things for as long as I can remember, and
-                                wrote my first program when I was 20 years old, just two weeks
-                                after my mom brought home the brand new Macintosh LC 550 that I
-                                taught myself to type on.
-                            </p>
-                            <p>
-                                The only thing I loved more than computers as a kid was space.
-                                When I was 8, I climbed the 40-foot oak tree at the back of our
-                                yard while wearing my older sister’s motorcycle helmet, counted
-                                down from three, and jumped — hoping the tree was tall enough
-                                that with just a bit of momentum I’d be able to get to orbit.
-                            </p>
-                            <p>
-                                I spent the next few summers indoors working on a rocket design,
-                                while I recovered from the multiple surgeries it took to fix my
-                                badly broken legs. It took nine iterations, but when I was 15 I
-                                sent my dad’s Blackberry into orbit and was able to transmit a
-                                photo back down to our family computer from space.
-                            </p>
-                            <p>
-                                Today, I’m the founder of Planetaria, where we’re working on
-                                civilian space suits and manned shuttle kits you can assemble at
-                                home so that the next generation of kids really <em>can</em>{' '}
-                                make it to orbit — from the comfort of their own backyards.
+                                My name is Sajjad Anwar, and I am a highly motivated and passionate software developer with [Number of Years] years of experience in the field. I have a strong background in GoLang,JavaScript,Python,TypeScript,PHP,
+                                Rust and have worked on a wide range of projects, from small scale website development to large-scale enterprise applications.
+
+                                Throughout my career, I have consistently demonstrated my ability to learn new technologies quickly, work well under pressure, and consistently deliver high-quality results. I have strong analytical and problem-solving skills and am committed to producing efficient and maintainable code.
+
+                                One of my key strengths is my ability to effectively communicate with both technical and non-technical stakeholders. I have experience working in a team environment and have consistently received positive feedback for my collaboration skills and ability to lead projects.
                             </p>
                         </div>
                     </div>
